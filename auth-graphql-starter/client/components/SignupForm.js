045951b1c0ff0ac9d7dc902f7a291b8e46a93446
Craig Newton <newtondev@gmail.com>
2018-06-13 10:25:21 +0200

    Added HOC auth component.

--- auth-graphql-starter/client/components/SignupForm.js
+++ auth-graphql-starter/client/components/SignupForm.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { graphql } from 'react-apollo';
+import { hashHistory } from 'react-router';
 import AuthForm from './AuthForm';
 import mutation from '../mutations/Signup';
 import query from '../queries/CurrentUser';
@@ -9,6 +10,12 @@ class SignupForm extends Component {
     super(props);
 
     this.state = { errors: [] };
+  }
+
+  componentWillUpdate(nextProps) {
+    if (nextProps.data.user && !this.props.data.user) {
+      hashHistory.push('/dashboard');
+    }
   }
 
   onSubmit({ email, password }) {
@@ -34,4 +41,6 @@ class SignupForm extends Component {
   }
 }
 
-export default graphql(mutation)(SignupForm);
+export default graphql(query)(
+  graphql(mutation)(SignupForm)
+);
